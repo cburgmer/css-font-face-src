9c2b670023505d873de1b88ed45f8e6048d254e6
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-11-05 22:36:04 +0800

    Test dist in phantomjs and include in Travis build

---
+++ test/loadPage.js
@@ -0,0 +1,16 @@
+var system = require('system');
+var fs = require('fs');
+var page = require('webpage').create();
+page.onConsoleMessage = function(msg) {
+    console.log(msg);
+};
+var path = system.args[1];
+if (path.indexOf('://') < 0) {
+    if (!fs.isAbsolute(path)) {
+        path = fs.workingDirectory + '/' + path;
+    }
+    path = 'file://' + path;
+}
+page.open(path, function () {
+    phantom.exit();
+});
